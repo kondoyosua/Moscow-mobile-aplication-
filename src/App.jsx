d6129a232338c99aead5195e96a9092d6ad7101d
kondoyosua <Suwaa2318@gmail.com>
2023-05-11 00:32:46 +0800

    Final Project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// screens
+import {
+  Splash,
+  Login,
+  Home,
+  Payment,
+  Account
+} from './screens';
+
+
+
+const Stack = createNativeStackNavigator();
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Splash' screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Splash' component={Splash} />
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Payment' component={Payment} />
+        <Stack.Screen name='Account' component={Account} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
---
+++ src/screens/index.js
@@ -0,0 +1,15 @@
+// screens
+
+import Splash from './Splash';
+import Login from './Login';
+import Home from './Home';
+import Payment from './Payment';
+import Account from './Account';
+
+export {
+  Splash,
+  Login,
+  Home,
+  Payment,
+  Account
+};
